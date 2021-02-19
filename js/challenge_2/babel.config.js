36e55fc4b799c5a3ab868c57b73ee57381d5bbdc
Sandeep Thukral <sandeepthukral@gmail.com>
2021-02-19 18:33:04 +0100

    introduced babel to enable ES6 features

---
+++ js/challenge_2/babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    presets: [
+        ['@babel/preset-env', {
+            targets: {
+                node: 14
+            }
+        }]
+    ],
+    "plugins": ['@babel/plugin-proposal-class-properties']
+}
