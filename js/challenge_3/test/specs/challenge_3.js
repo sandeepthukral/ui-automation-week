023f14901778a21ef97d5f9e0964a7a478ed4150
Sandeep Thukral <sandeepthukral@gmail.com>
2021-02-21 16:33:53 +0100

    waiting for the map to load before visual check

--- js/challenge_3/test/specs/challenge_3.js
+++ js/challenge_3/test/specs/challenge_3.js
@@ -12,29 +12,35 @@ describe('Challenge 3 Tests', () => {
         {width: 1200, height: 1013, type: 'desktop'}
     ].forEach(testData => {
         it(`should check the home page images for viewport ${testData.type}`, () => {
+            // setup the mock for the network calls for the map
+            const mockMapRequests = browser.mock('**/**fastly.net/**')
+
             browser.setWindowSize(testData.width, testData.height);
     
             HomePage.visit();
-            browser.waitUntil(
-                () => HomePage.imgMaps.length === 16
-            )
-            browser.pause(500);
-            expect(browser.checkScreen('Homepage')).toEqual(0);
-    
+            
             // visual regression for the hotel logo
             HomePage.imgHotelLogo.scrollIntoView();
-            browser.pause(500);
             expect(browser.checkElement(HomePage.imgHotelLogo, 'Homepage image logo')).toEqual(0);
             
             // visual regression for the first room
             // it has been observed that the first room is seeded on every refresh of the DB
             HomePage.firstRoom.scrollIntoView();
-            browser.pause(500);
             expect(browser.checkElement(HomePage.firstRoom, 'Homepage - first room')).toEqual(0);
-    
+            
             // visual regression for the map
             HomePage.map.scrollIntoView();
-            browser.pause(500);
+            // wait for the map to load completely
+            // wait first for calls for the map to be created
+            browser.waitUntil(() => {
+                console.log(`Number of mocked calls ... ${mockMapRequests.calls.length}`)
+                return mockMapRequests.calls.length === 15
+            })
+            // wait for all the calls to complete
+            browser.waitUntil(() => {
+                const statusCodes = mockMapRequests.calls.map(call => call.statusCode);
+                return statusCodes.every(statusCode => statusCode === 200);
+            })
             expect(browser.checkElement(HomePage.map, 'Homepage - map')).toEqual(0);
         });
     })
